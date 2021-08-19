6b362bb93e251b739e77533ed54a4387e0506e28
czhc <cedriczhchan@gmail.com>
2021-08-19 11:31:57 +0800

    Cleanup ignored files

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-/Users/cedric/Documents/Projects/rust-intro/hello_world/target/debug/deps/hello_world: src/main.rs
-
-/Users/cedric/Documents/Projects/rust-intro/hello_world/target/debug/deps/hello_world.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/cedric/Documents/Projects/rust-intro/hello_world/target/debug/hello_world: /Users/cedric/Documents/Projects/rust-intro/hello_world/src/main.rs
