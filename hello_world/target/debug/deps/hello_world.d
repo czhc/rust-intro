ea029f4e6efd7f75901b165d442b23842edfb5b0
czhc <cedriczhchan@gmail.com>
2021-08-19 11:26:28 +0800

    Initial commit

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/cedric/Documents/Projects/rust-intro/hello_world/target/debug/deps/hello_world: src/main.rs
+
+/Users/cedric/Documents/Projects/rust-intro/hello_world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
